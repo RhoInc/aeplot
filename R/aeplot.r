543e52ba402d6243f0caf99c844d041a7c36c385
Jeremy Wildfire <jwildfire@gmail.com>
2016-03-18 07:55:23 -0400

    aeplot now returns a lattice plot (instead of printing a pdf)
    also switched to stringr for trim function

--- R/aeplot.r
+++ R/aeplot.r
@@ -1,60 +1,6 @@
 ##################################################################################
-#                                aeplot() Documentation
-# (1) Overview
-# AEplot() is a function that generates summary Plots of AE Prevelance using lattice (in R)
-#
-# (2) File Summary
-# Input: CSV file (1 record per AE + a header row) with the following 4 columns:
-# 1. ID (text or numeric)
-# 2. SystemOrganClass (text)
-# 3. PreferredName(text)
-# 4. Group (text)
-#
-# Output: Summary figure saved as a pdf
-#
-# (3) Funtion Arguements (default) - details.
-# Note: The 3 required arguments are "groups", "ngroups", and "pathin"
-#
-# 3.1 Metadata
-# groups (None - REQUIRED) -  list of the groups to be included in the plot. wording must match the raw data.
-#                             e.g. c("placebo","treatment")
-#
-# ngroups (None - REQUIRED) - # of participants in each group. Array of Numeric with same # of elements as /groups/.
-#                             order should match /groups/. If groups is not specified, the values should be listed
-#                             in alphabetical order according to group name. e.g. c(149,147)
-#
-# 3.2 Graphic options (All for pdf only. Interactive version allows many of these options to be toggled in the webpage)
-# cut (0) - only include terms with prevalence >= /cut/
-# cut_organ_class (FALSE) - apply cut to organ class as well as pref name?
-#                         if F all systemOrganClasses will be shown regardless of /cut/.
-#                         if T,  they will be removed according to /cut/.
-# numerator ("pt") - What should the numerator for your rates be? "pt" = participants w. 1+ ae , "ae" = total # of AEs
-# colors (see details) - group colors default to "set1" in RColorBrewer, but can be specified using c("#200","#020",etc...)
-# grouplabels (/groups/) - how should the group names be printed in the column header.
-#
-# 3c. Input/Output
-# pathin ("None - REQUIRED") - name of the csv  containing the 1 record per AE data set
-# pathout (paste(getwd(),"fig/AEplot.pdf")) - path where the pdf figure is stored
-#
-# (4) Sample Function Calls
-# 4a. Basic Plot with defaults. Includes all observered organ classes & prefferred names
-# AEPlot(pathin="O:/Asthma/VizLibrary/AEfedex/data/ACE.csv",                        #Required
-#       groups=c("intervention","placebo"),ngroups=c(280,272))                      #Required
-#
-# 4b. Customized plot with custom title and group names. Includes classes & preferred names >= 3%.
-# AEPlot(pathin="O:/Asthma/VizLibrary/AEfedex/data/ace.csv",                        #Required
-#       groups=c("intervention","placebo"),ngroups=c(280,272),                      #Required
-#       pathout="O:/Asthma/VizLibrary/AEfedex/fig/AEPlot_3percent.csv",             #Optional
-#       grouplabel=c("FeNO Group","Non-FeNO Group"),                                #Optional
-#       cut=3,cut_organ_class=F,title="AE Prevelance in ACE (3% or greater)")         #Optional
-#
-# (5) Change Log
-# 10Oct2011 - jwildfire - Version 1.0 released.
-# 13Jan2014 - jwildfire - bugfix for certain sort patterns addressed (see line 99)
-# 17Mar2016 - jwildfire - updates for R package
+# aeplot() - generates summary Plots of AE Prevelance using lattice (in R)
 ###########################################################################################
-
-
 
 aeplot <- function(
   data,
@@ -102,11 +48,8 @@ aeplot <- function(
   sym <- c(1:groupcount)                                 #Symbols
 
   ## Create the pdf
-  pdf(file=pathout, height=11, width=8.5)
   lattice::trellis.par.set("axis.line", list(col=NA, lty=1, lwd=1))
-
-  print(
-    lattice::xyplot(index~max.percent | page, data = plot.data,
+  thisPlot<-lattice::xyplot(index~max.percent | page, data = plot.data,
            main = title,
            layout = c(1,1),
            as.table = T,
@@ -130,7 +73,7 @@ aeplot <- function(
                pos = 4,
                cex = 0.8,
                font = ifelse(
-                 trim(current$pref) == "All",
+                 stringr::str_trim(current$pref) == "All",
                  2,
                  1
                )
@@ -151,7 +94,7 @@ aeplot <- function(
                y0 = min(y) - 0.7, y1= max(y) + 0.5, col = "gray80"
              )
 
-             lattice::panel.abline(h = y[trim(current$pref) == "All"] - 0.5)
+             lattice::panel.abline(h = y[stringr::str_trim(current$pref) == "All"] - 0.5)
              lattice::panel.abline(h = c(max(y) + 0.5, -0.5))
 
              group.num <- 0
@@ -179,7 +122,7 @@ aeplot <- function(
                anno <- paste(
                  current[,c(paste(i,".n",sep=""))],
                  " (",
-                 trim(
+                 stringr::str_trim(
                    format(
                      round(
                        current[ ,c(paste(i,".percent",sep=""))],
@@ -201,6 +144,5 @@ aeplot <- function(
              }
            }
     )
-  )
-  dev.off()
+    return(thisPlot)
 }
